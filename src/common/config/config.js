2d9ee257f646ff59e8134e3679dbaa9017d3d19f
qywuyanzu <1806804351@qq.com>
2019-02-26 19:39:17 +0800

    fix BUG

--- src/common/config/config.js
+++ src/common/config/config.js
@@ -4,6 +4,6 @@ config.version = "1.1.2";
 
 config.title = "清技背单词";
 
-config.server_url = 'http://192.168.1.199/learn_english/server/public/';
+config.server_url = 'http://127.0.0.1/learn_english/server/public/';
 
 export default config;
